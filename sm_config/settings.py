c7b375a2344387043191b49ec7c6433625244a17
mmac.pers@gmail.com <michael@sixtymill.com>
2017-11-19 13:52:51 -0600

    added gzip middleware

--- sm_config/settings.py
+++ sm_config/settings.py
@@ -86,6 +86,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.gzip.GZipMiddleware', #added to reduce initial response size
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
